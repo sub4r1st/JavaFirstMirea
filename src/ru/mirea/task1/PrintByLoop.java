39a38418b3c0a01eebd5345e5ad941f31f3750d9
sub4r1st <subarist.dima@gmail.com>
2021-09-16 13:29:59 +0300

    lab1

--- src/ru/mirea/task1/PrintByLoop.java
+++ src/ru/mirea/task1/PrintByLoop.java
@@ -2,7 +2,7 @@ package ru.mirea.task1;
 
 import java.util.Scanner;
 
-public class PrintArgs {
+public class PrintByLoop {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         for (int i = 1; i < 6; i++){
